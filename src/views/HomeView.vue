a53a82f7671eb1c5297eabd253c412ee5b286f76
root <root@LAPTOP-MR6ODUSS>
2023-09-29 10:58:08 +0200

    feat:add Actors, Movies and Categories components and View

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,22 @@
 <script setup>
-//import data from 'http://localhost:8080/symfonyS5/public/index.php/api/movies';
 import { onMounted, ref } from 'vue'
 import axios from 'axios'
+import Actors from "@/components/Actors.vue";
+import Movies from "@/components/Movies.vue";
 
-
-let response = ref('')
+let responseMovies = ref('')
+let responseActors = ref('')
 let movies = ref('')
+let actors = ref('')
+
 onMounted(async () => {
-  const response = await axios.get('http://localhost:8080/symfonyS5/public/index.php/api/movies');
-  movies.value = response.data['hydra:member'];
-  console.log(movies.value);
+  const responseMovies = await axios.get('http://localhost:8080/symfonyS5/public/index.php/api/movies?pagination=false');
+  movies.value = responseMovies.data['hydra:member'].reverse();
+  movies.value = movies.value.slice(0,4);
+
+  const responseActors = await axios.get('http://localhost:8080/symfonyS5/public/index.php/api/actors?pagination=false');
+  actors.value = responseActors.data['hydra:member'].reverse();
+  actors.value = actors.value.slice(0,4)
 });
 
 </script>
@@ -18,10 +25,46 @@ onMounted(async () => {
 <h1>HomePage</h1>
   <div class="wrapper">
     <div class="movies">
-      <div class="movie" v-for="movie in movies" :key="movie.id">
-        <h2>{{ movie.title }}</h2>
-        <p>{{ movie.description }}</p>
+      <h2>Last 4 movies</h2>
+      <div class="last-four">
+        <Movies v-for="movie in movies"
+                :key="movie.id"
+                :title="movie.title"
+                :description="movie.description"
+                :releaseDate="movie.releaseDate"
+                :duration="movie.duration"
+                :category="movie.category.name"
+                :actors="movie.actor"
+        />
+      </div>
+    </div>
+    <div class="actors">
+      <h2>The last 4 actors</h2>
+      <div class="last-four">
+        <Actors v-for="actor in actors"
+                :key="actor.id"
+                :firstName="actor.firstName"
+                :lastName="actor.lastName"
+                :nationality="actor.nationality.nationality"
+        />
       </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.last-four {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+}
+
+.card {
+  width: 30%;
+  padding: 1rem;
+  margin: 1rem;
+  border: 1px solid var(--color-border);
+  border-radius: 4px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+</style>
