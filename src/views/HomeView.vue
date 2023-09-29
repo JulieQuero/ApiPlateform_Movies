0e39b3a5c6b1a3b3a87a577c9389514eea47db9c
root <root@LAPTOP-MR6ODUSS>
2023-09-29 08:46:33 +0200

    feat: add HomePage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,27 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+//import data from 'http://localhost:8080/symfonyS5/public/index.php/api/movies';
+import { onMounted, ref } from 'vue'
+import axios from 'axios'
+
+
+let response = ref('')
+let movies = ref('')
+onMounted(async () => {
+  const response = await axios.get('http://localhost:8080/symfonyS5/public/index.php/api/movies');
+  movies.value = response.data['hydra:member'];
+  console.log(movies.value);
+});
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+<h1>HomePage</h1>
+  <div class="wrapper">
+    <div class="movies">
+      <div class="movie" v-for="movie in movies" :key="movie.id">
+        <h2>{{ movie.title }}</h2>
+        <p>{{ movie.description }}</p>
+      </div>
+    </div>
+  </div>
 </template>
